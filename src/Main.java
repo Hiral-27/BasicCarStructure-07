b89ff95101be8585debe60e404ffcdbf9cda2c4f
Hiral-27 <hiral27496@gmail.com>
2023-03-29 21:25:35 -0400

    The color feature has been added

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,10 @@ public class Main {
         x.size="1234cc";
         x.weight="6453k";
 
+        Color col= new Color();
+        col.colorLights="True";
+        col.colorTyre="false";
+        col.colorWindows="True";
+
     }
 }
